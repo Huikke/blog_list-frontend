51b7afec829014e6eb8b45e5868a45b0a8e982c8
Lassi Chen <lachen@utu.fi>
2023-04-15 19:54:54 +0300

    5.2 done

--- src/App.js
+++ src/App.js
@@ -15,17 +15,35 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = async event => {
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password
       })
-      console.log(user)
+
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
       setUser(user)
+      setUsername('')
+      setPassword('')
     } catch (exception) {
       console.log(exception)
     }
+  }
+
+  const handleLogout = event => {
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
   }
 
   if (user === null) {
@@ -60,7 +78,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button onClick={handleLogout}>logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
