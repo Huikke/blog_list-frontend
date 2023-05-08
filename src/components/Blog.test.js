81dfe534ecdbd845d86634181f8551bebf68d4a2
Lassi Chen <lachen@utu.fi>
2023-05-08 04:34:00 +0300

    5.12 done

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
-	const blog = {
-		title: 'Rantakeli',
-		author: 'Kalle Rantanen',
-		likes: 1,
-		user: {
-			username: 'testuser'
-		}
-	}
-
-	const user = {
+const blog = {
+	title: 'React testing is something else',
+	author: 'Pate',
+	likes: 1,
+	user: {
 		username: 'testuser'
 	}
+}
 
+const user = {
+	username: 'testuser'
+}
+
+const mockAddLike = jest.fn()
+
+test('renders content', () => {
 	render(<Blog blog={blog} user={user} />)
 
-	const element = screen.getByText('Rantakeli | Kalle Rantanen | likes: 1')
+	const element = screen.getByText('React testing is something else | Pate | likes: 1')
 	expect(element).toBeDefined()
+})
+
+test('click the like button twice calls event handler twice', async() => {
+	render(<Blog blog={blog} user={user} addLike={mockAddLike} />)
+
+	const clicker = userEvent.setup()
+	const button = screen.getByText('like')
+	await clicker.click(button)
+	await clicker.click(button)
+
+	expect(mockAddLike.mock.calls).toHaveLength(2)
 })
